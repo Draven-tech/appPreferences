bf6dd9afc6ab5d52a727d833d2ea272732fe81cf
jspe2bb04 <jspe2bb04@scs.local>
2025-03-12 00:34:38 -0700

    2nd version

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,6 +8,25 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  option1: boolean = false;
+  option2: boolean = false;
+  option3: boolean = false;
+  value: number = 50;
+  checkbox1: boolean = false;
+  checkbox2: boolean = false;
+  checkbox3: boolean = false;
+  radio1: string = '';
+  radio2: string = '';
 
+  resetSettings() {
+    this.option1 = false;
+    this.option2 = false;
+    this.option3 = false;
+    this.value = 50;
+    this.checkbox1 = false;
+    this.checkbox2 = false;
+    this.checkbox3 = false;
+    this.radio1 = '';
+    this.radio2 = '';
+  }
 }
